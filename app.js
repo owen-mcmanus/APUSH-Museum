8b67386d2c18d2cada2a90bd61a92a8452055b62
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-05-21 17:44:19 -0700

    add details and ice houses

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ let r = 41;
 let b = 47;
 let g = 54;
 function scrollFunction() {
-  if (document.documentElement.scrollTop < 770) {
+  let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+  if (scrollTop < 900) {
     navbar.style.backgroundColor  = "rgba(41, 47, 54, 0.15)";
     navbar.style.color  = "rgb(41, 47, 54)";
     r = 41
     b = 47
     g = 54
-  }else if(document.documentElement.scrollTop < 1250){
-    let scrollpos = (document.documentElement.scrollTop - 770)/478
+  }else if(scrollTop < 1000){
+    let scrollpos = (scrollTop - 900)/100
 
     navbar.style.backgroundColor  = "rgba(41, 47, 54," + (scrollpos+.15)/1.15 + ")";
     r = scrollpos*(255-41)+41
@@ -19,7 +21,7 @@ function scrollFunction() {
     g = scrollpos*(255-54)+54
 
     navbar.style.color  = `rgb(${r}, ${b}, ${g})`;
-  }else { // 1250
+  }else { 
     navbar.style.backgroundColor  = "rgba(41, 47, 54, 1)";
     navbar.style.color  = "#ffffff";
   }
