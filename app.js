290217ec947b23ebb22a1b1e611b7ce927d0df11
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-05-22 20:46:06 -0700

    trolling Mr.P

--- app.js
+++ app.js
@@ -26,3 +26,15 @@ function scrollFunction() {
     navbar.style.color  = "#ffffff";
   }
 }
+
+
+
+
+let btn = document.getElementById("close");
+let awsBanner = document.getElementById("AWS");
+
+btn.addEventListener("click", (e)=>{
+awsBanner.style.display = "none";
+e.stopPropagation();
+});
+awsBanner.addEventListener("click", ()=>location.href = "https://www.amazon.com/");
